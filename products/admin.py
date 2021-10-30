c714537d7d402163bf447c1c471cf35e40a1f75a
Stephen <sjohnston.ie@gmail.com>
2021-10-30 17:06:17 +0000

    feat: Add classes ProductAdmin and CategoryAdmin to admin.py within products app

--- products/admin.py
+++ products/admin.py
@@ -1,3 +1,28 @@
 from django.contrib import admin
+from .models import Product, Category
 
 # Register your models here.
+
+# Below is a tuple change the list order below to change order in which they 
+# are displayed on the site
+class ProductAdmin(admin.ModelAdmin):
+    list_display = (
+        'sku',
+        'name',
+        'category',
+        'price',
+        'rating',
+        'image',
+    )
+
+    ordering = ('sku',)
+    # To reverse the order put a - infront of sku
+
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = (
+        'friendly_name',
+        'name',
+    )
+
+admin.site.register(Product, ProductAdmin)
+admin.site.register(Category, CategoryAdmin)
