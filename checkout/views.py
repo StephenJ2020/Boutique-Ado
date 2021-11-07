faedc2fd6b39ce559175e5d5eaaf092ed21baaaa
Stephen <sjohnston.ie@gmail.com>
2021-11-07 14:56:46 +0000

    feat: Add basic checkout payment functionality

--- checkout/views.py
+++ checkout/views.py
@@ -9,8 +9,8 @@ import stripe
 
 
 def checkout(request):
-    # stripe_public_key = settings.STRIPE_PUBLIC_KEY
-    # stripe_secret_key = settings.STRIPE_SECRET_KEY
+    stripe_public_key = settings.STRIPE_PUBLIC_KEY
+    stripe_secret_key = settings.STRIPE_SECRET_KEY
 
     bag = request.session.get('bag', {})
     if not bag:
@@ -20,20 +20,25 @@ def checkout(request):
     current_bag = bag_contents(request)
     total = current_bag['grand_total']
     stripe_total = round(total * 100)
-    # stripe.api_key = stripe_secret_key
-    # intent = stripe.PaymentIntent.create(
-    #     amount=stripe_total,
-    #     currency=settings.STRIPE_CURRENCY,
-    # )
+    stripe.api_key = stripe_secret_key
+    intent = stripe.PaymentIntent.create(
+        amount=stripe_total,
+        currency=settings.STRIPE_CURRENCY,
+    )
 
-    # print(intent)
+    # print(intent) for testing purposes - handy to see the attributes within the intent dictionary
 
     order_form = OrderForm()
+
+    if not stripe_public_key:
+        messages.warning(request, 'Stripe public key is missing. \
+            Did you forget to set it in your environment?')
+
     template = 'checkout/checkout.html'
     context = {
         'order_form': order_form,
-        'stripe_public_key': 'pk_test_51JstpWEDjuJ37iFpPKaXgdToDLPIHrR98xgsFHl8X7OKKryQ231RLzjHWNoM20SHlW8S0HpAsIPj8DGqqSsPWxHN00X8nwEyyW',
-        'client_secret': 'test client secret',
+        'stripe_public_key': stripe_public_key,
+        'client_secret': intent.client_secret,
     }
 
     return render(request, template, context)
